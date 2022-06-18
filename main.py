21b0c294a2b34ae3ad2d4e20fb91ffec8bf6f14a
Nikita <nikita1number@gmail.com>
2022-06-19 00:39:04 +0300

    чекайте мужики

--- main.py
+++ main.py
@@ -1,20 +1,35 @@
 import hashlib
+import pandas as pa
 
-salt = 1
-cnt = 0
+table = pa.read_excel('table.xlsx', sheet_name='hashes')
+values = table['Строки'].tolist()
+
 hex_value = ''
+positions = [[252, 256], [222, 226], [192, 196], [162, 166], [132, 136],
+             [102, 106], [72, 76], [42, 46], [12, 16], [0, 4]]
 
-value = input('Введите строку: ')
+for n in range(10):
+    iterations = []
+    for value in values:
+        cnt = 0
+        salt = 0
+        print(f'Берем первую строку: {value}')
+        for pos in positions:
+            while hex_value[pos[0]:pos[1]] != '0119':
+                cnt += 1
+                salt += 1
+                hex_value = hashlib.pbkdf2_hmac('sha256',
+                                                value.encode('utf-8'),
+                                                salt.to_bytes(16, byteorder='little'),
+                                                10000,
+                                                dklen=128).hex()
+                print(f'Количество итераций: {cnt} \n'
+                      f'Хэш: {hex_value} \n'
+                      f'4 символа: {hex_value[pos[0]:pos[1]]}')
+            print(f'Полученный хэш: {hex_value}\n'
+                  f'Количество итераций: {cnt}')
+            iterations.append(cnt)
+    iterations.append(sum(iterations) // len(iterations))
+    table.insert(table.shape[1], f'{pos}', iterations)
 
-while hex_value[-4:] != '0119':
-    hex_value = hashlib.pbkdf2_hmac('sha256',
-                                    value.encode('utf-8'),
-                                    salt.to_bytes(16, byteorder='little'),
-                                    10000,
-                                    dklen=128).hex()
-    salt += 1
-    cnt += 1
-    print(f'Количество итераций: {cnt}')
-    print(f'Последние 4 символа хэша: {hex_value[-4:]}\n')
-
-print(f'Полученный хэш: {hex_value}')
+table.to_excel('table.xlsx', sheet_name='hashes')
